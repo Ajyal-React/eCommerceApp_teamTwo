f8b26cadf9cda512afd28e3bd57d11d9c8cdf87d
Mona Mohammed <mona00mona1997@gmail.com>
2022-06-13 01:18:14 +0300

    Global-Style

---
+++ src/Global.style.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.main`
+  width: 100vw;
+  max-width: 1920;
+  margin: 0 auto;
+  padding: 0;
+`;
+
+export const InnerContainer = styled.div`
+  width: 80%;
+  margin: auto;
+`;
+
+export const FlexBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Button = styled.button`
+  background-color: ${(props) => props.backgroundColor};
+  border-radius: ${(props) => props.borderR};
+  border: none;
+`;
+
+export const MainHeader = styled.h1`
+  letter-spacing: -2.53px;
+  color: #ffffff;
+  text-transform: uppercase;
+`;
+
+export const Title = styled.h2`
+  font-size: 30px;
+`;
+
+export const Paragraphe = styled.p`
+  font-size: ${(props) => props.fontSize};
+`;
+
+
