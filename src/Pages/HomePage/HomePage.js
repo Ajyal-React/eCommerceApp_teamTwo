106b4962a32121b756483140e2f5df47132497e9
EmanHass <eman_hassouna@hotmail.com>
2022-06-13 18:20:07 +0300

    Used components to display header and button and use styled to make backgroundImage

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import HeroSection from '../../Components/HeroSection/HeroSection'
 import MainNavbar from '../../Components/Navbar/MainNavbar'
-import { MainContainer } from '../../Global.style'
+import { MainContainer, MainHeader, Button} from '../../Global.style'
+import {BackgroundImage,UpdateFlexBox} from './HomePage.Style'
 
 function HomePage() {
   return (
     <MainContainer>
       <MainNavbar/>
       <HeroSection/>
+
+      
+      <BackgroundImage>
+        <UpdateFlexBox>
+          <MainHeader fontSize='20px'>get 50% off</MainHeader>
+          <Button
+           backgroundColor='none' borderR='5px' border='1px solid' padding='10px 20px' color='#ffff'>
+            start Shopping</Button>
+        </UpdateFlexBox>
+      </BackgroundImage>
+
     </MainContainer>
   )
 }
