3c377e951e1f5e0a5fd54a20bc76323ab3b0ca05
EmanHass <eman_hassouna@hotmail.com>
2022-06-13 20:22:50 +0300

    edit styled for image

--- src/Pages/HomePage/HomePage.Style.js
+++ src/Pages/HomePage/HomePage.Style.js
@@ -3,10 +3,9 @@ import backImg from "../../images/Group14858.png";
 import {FlexBox} from '../../Global.style'
 
 export const BackgroundImage = styled.div`
-    height: 1088px;
     background-image: url(${backImg});
     background-repeat: no-repeat;
-    background-size: contain;
+    background-size: cover;
 `
 export const UpdateFlexBox = styled(FlexBox)`
     justify-content: space-around;
