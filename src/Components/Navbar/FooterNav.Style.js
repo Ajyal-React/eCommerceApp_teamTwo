96c8244cd87a907bb01f61dff94101defe1c42f9
mariamabunada <mariamabunada2000@gmail.com>
2022-06-16 18:58:12 +0300

    create footer and it's style relate #13

---
+++ src/Components/Navbar/FooterNav.Style.js
@@ -0,0 +1,57 @@
+import styled from "styled-components"
+import { Paragraphe, Title } from './../../Global.style';
+
+export const FooterContainer=styled('div')`
+height: 333px;
+
+`
+export const FirstSide=styled('div')`
+width: 414px;
+`
+
+export const LogoImg = styled('Img')`
+width: 188px;
+height: 39px;
+`
+
+export const FooterText=styled(Paragraphe)`
+max-width: 414px;
+font-size:12px;
+color:#302F2F;
+text-align: left;
+`
+export const SecondSide=styled('div')`
+
+`
+
+export const FooterTitle=styled(Title)`
+font-size:12px;
+color:#302F2F;
+`
+
+export const FooterSubTitle=styled(Paragraphe)`
+font-size:12px;
+color:#646363;
+`
+export const MenuContainer=styled('div')`
+margin:2vh;
+`
+export const InfoContainer=styled('div')`
+
+`
+export const ThirdSide=styled('div')`
+
+`
+export const LastSide=styled('div')`
+
+`
+export const SocialIcons=styled('div')`
+
+`
+export const SocialStyle=styled('div')`
+padding:1vh;
+`
+
+
+
+
