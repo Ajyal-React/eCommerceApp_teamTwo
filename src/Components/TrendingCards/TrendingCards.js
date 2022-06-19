1dd30717ac68d4888ad8d3f11218df09f67fcdc3
mariamabunada <mariamabunada2000@gmail.com>
2022-06-19 12:54:30 +0300

    edit trending page cards

--- src/Components/TrendingCards/TrendingCards.js
+++ src/Components/TrendingCards/TrendingCards.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Paragraphe, FlexBox } from "./../../Global.style";
 import { TrendingCard, TrendingImg } from "./TrendingCards.style";
-import image from "../../images/Base.png";
+import TrendingPage from "../../Pages/TrendingPage/TrendingPage";
+// import image from "../../images/Base.png";
 
-function TrendingCards() {
+function TrendingCards({name, price, image}) {
   return (
     <TrendingCard>
       <TrendingImg src={image}></TrendingImg>
       <FlexBox>
         <Paragraphe fontSize="14px" color="111111">
-          Nike Court Air
+          {name}
         </Paragraphe>
         <Paragraphe fontSize="12px" color="#FF4F04">
-          $58
+         {price}
         </Paragraphe>
       </FlexBox>
     </TrendingCard>
