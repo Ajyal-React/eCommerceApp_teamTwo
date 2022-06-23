49620e99b738907653381ce4ffbb00ad4d73374e
mariamabunada <mariamabunada2000@gmail.com>
2022-06-23 09:21:03 +0300

    add offer page request relates #27

--- src/Components/HeroSection/HeroSection.js
+++ src/Components/HeroSection/HeroSection.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import {
   MainHeader,
   Paragraphe,
@@ -11,14 +11,28 @@ import {
   MdDoubleArrowIcon,
   RiArrowDownSLineIcon,
 } from "./HeroSection.Style";
+import axios from 'axios';
 
 function HeroSection() {
+  const [data, getData] = useState([]);
+  
+  useEffect(() => {
+    const OffersGet=async()=>{
+      const res=await axios.get('https://omar-tech-store.herokuapp.com/api/products/offers')
+      getData(res.data[0])
+    
+    
+    
+    }
+   
+    OffersGet()
+  }, []);
   return (
-    <MainHero>
+    <MainHero image={data.imageBanner}>
       <InnerContainerHero>
-        <MainHeader letterSpacing='.5px'>PLAYSTATION 5</MainHeader>
+        <MainHeader letterSpacing='.5px'>{data.name}</MainHeader>
         <Paragraphe fontSize="20px" color="#fff">
-          lightning-fast download speed with super-fast ssd storage
+          {data.description}
         </Paragraphe>
         <ButtonHero>
           Start Shopping <MdDoubleArrowIcon />
